3acd8adf4b03ccf2c8b196911d7244ce41a162e7
alcorerhythm <kuhakusama3@gmail.com>
2024-12-11 17:29:38 +0700

    init inventory & fixing cosmetic

---
+++ assets/js/system/battle/inventory.js
@@ -0,0 +1,11 @@
+let chooseInvenotyItem = false;
+
+function loadInventory(){
+    $("#inventoryPanel").show();
+    callInventoryList();
+
+}
+
+function callInventoryList(){
+    console.log("openInventory!")
+}
